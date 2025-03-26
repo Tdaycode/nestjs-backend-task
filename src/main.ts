b5bdb053c2060df4cd35acfae37f61b5a0367cbc
Omotayo Ganiyu <ganiyuomotayo2000@gmail.com>
2025-03-26 16:58:36 +0100

    Chore: final submission

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { rateLimit } from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       whitelist: true,
+    }),
+  );
+
+  app.use(
+    '/graphql',
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100,
+      message: 'Too many requests, please try again later.',
+      standardHeaders: true,
+      legacyHeaders: false,
     }),
   );
 
